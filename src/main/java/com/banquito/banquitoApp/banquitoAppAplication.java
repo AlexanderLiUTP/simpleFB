d8985212ec483c4c13156d80fe0c806f05314d0d
Alexander Li <alex.liq0225@gmail.com>
2023-04-21 18:04:02 +0000

    Add: inicializador de la bd al proyecto spring

--- src/main/java/com/banquito/banquitoApp/banquitoAppAplication.java
+++ src/main/java/com/banquito/banquitoApp/banquitoAppAplication.java
@@ -1,5 +1,7 @@
 package com.banquito.banquitoApp;
 
+import com.banquito.banquitoApp.utils.db.testDBInit;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,8 @@ import javax.ws.rs.ApplicationPath;
 @SpringBootApplication
 @ApplicationPath("/api/v1")
 public class banquitoAppAplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException{
+        testDBInit.init();
         SpringApplication.run(banquitoAppAplication.class, args);
     }
 }
